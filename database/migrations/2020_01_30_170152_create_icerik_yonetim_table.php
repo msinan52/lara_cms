9a6ec7322f3ab1a8ac50341683d1f5bdba54463a
muratenes <m4urat@gmail.com>
2020-01-31 19:58:48 +0300

    migrations

---
+++ database/migrations/2020_01_30_170152_create_icerik_yonetim_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIcerikYonetimTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('icerik_yonetim', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100);
+            $table->string('image', 200)->nullable();
+            $table->string('slug', 130);
+            $table->string('spot', 255)->nullable();
+            $table->text('desc');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('icerik_yonetim');
+    }
+}
